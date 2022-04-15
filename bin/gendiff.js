a063cf12c10a52eee38a7da41fce24550d79c055
Frit027 <Frit_027@mail.ru>
2022-04-15 14:47:54 +0300

    added option and arguments

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,6 +3,8 @@ import { program } from 'commander';
 
 program
   .version('0.0.1')
+  .option('-f, --format <type>', 'output format')
+  .arguments('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.');
 
 program.parse();
