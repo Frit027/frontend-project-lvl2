5234b545effc4380e3d68e98092627da8eeea970
Frit027 <Frit_027@mail.ru>
2022-05-21 16:26:58 +0300

    added action, changed version

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,12 @@
 #!/usr/bin/env node
 import { program } from 'commander';
+import genDiff from '../src/index.js';
 
 program
-  .version('0.0.1')
+  .version('1.0.0')
   .option('-f, --format <type>', 'output format')
   .arguments('<filepath1> <filepath2>')
-  .description('Compares two configuration files and shows a difference.');
+  .description('Compares two configuration files and shows a difference.')
+  .action(genDiff);
 
 program.parse();
