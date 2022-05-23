4b8581a60e50fc8f3a4d1f8466e8a8941ae5d162
Frit027 <Frit_027@mail.ru>
2022-05-23 17:59:49 +0300

    added tests for yaml

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,24 +1,26 @@
-import { dirname, join } from 'path';
-import { readFileSync } from 'fs';
+import path from 'path';
 import { fileURLToPath } from 'url';
 import genDiff from '../src/index.js';
+import { readFile } from '../src/utilities.js';
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
-const getFixturePath = (filename) => join(__dirname, '..', '__fixtures__', filename);
-const readFile = (filename) => readFileSync(getFixturePath(filename), 'utf-8');
-
-const files = {
-  'result1.txt': ['file1.json', 'file2.json'],
-  'result2.txt': ['file3.json', 'file4.json'],
-  'result3.txt': ['file5.json', 'file6.json'],
+const data = {
+  'result1.txt': [['file1.json', 'file2.json'], ['file1.yml', 'file2.yaml']],
+  'result2.txt': [['file3.json', 'file4.json']],
+  'result3.txt': [['file5.json', 'file6.json']],
 };
 
-test('JSON comparison', () => {
-  Object.keys(files).forEach((resultTXT) => {
-    const result = readFile(resultTXT).replace(/\r\n/g, '\n');
-    const diff = genDiff(getFixturePath(files[resultTXT][0]), getFixturePath(files[resultTXT][1]));
-
-    expect(diff).toBe(result);
+test('File comparison', () => {
+  Object.keys(data).forEach((txtFile) => {
+    const result = readFile(getFixturePath(txtFile)).replace(/\r\n/g, '\n');
+    data[txtFile].forEach((files) => {
+      const diff = genDiff(
+        getFixturePath(files[0]),
+        getFixturePath(files[1]),
+      );
+      expect(diff).toBe(result);
+    });
   });
 });
