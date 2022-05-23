01921dc1aa48cb3dfae22da00e71c07c5d18bca1
Frit027 <Frit_027@mail.ru>
2022-05-23 17:59:35 +0300

    added parser

--- src/index.js
+++ src/index.js
@@ -1,26 +1,22 @@
 import _ from 'lodash';
-import fs from 'fs';
-import path from 'path';
+import parser from './parser.js';
 
 export default (file1, file2) => {
-  const data1 = fs.readFileSync(path.resolve(file1), 'utf-8');
-  const data2 = fs.readFileSync(path.resolve(file2), 'utf-8');
-
-  const json1 = JSON.parse(data1);
-  const json2 = JSON.parse(data2);
-  const keys = _.uniq([...Object.keys(json1), ...Object.keys(json2)]).sort();
+  const data1 = parser(file1);
+  const data2 = parser(file2);
+  const keys = _.uniq([...Object.keys(data1), ...Object.keys(data2)]).sort();
   const arr = [];
 
   keys.forEach((key) => {
-    if (key in json1 && !(key in json2)) {
-      arr.push(`  - ${key}: ${json1[key]}`);
-    } else if (!(key in json1) && key in json2) {
-      arr.push(`  + ${key}: ${json2[key]}`);
-    } else if (json1[key] !== json2[key]) {
-      arr.push(`  - ${key}: ${json1[key]}`);
-      arr.push(`  + ${key}: ${json2[key]}`);
+    if (key in data1 && !(key in data2)) {
+      arr.push(`  - ${key}: ${data1[key]}`);
+    } else if (!(key in data1) && key in data2) {
+      arr.push(`  + ${key}: ${data2[key]}`);
+    } else if (data1[key] !== data2[key]) {
+      arr.push(`  - ${key}: ${data1[key]}`);
+      arr.push(`  + ${key}: ${data2[key]}`);
     } else {
-      arr.push(`    ${key}: ${json1[key]}`);
+      arr.push(`    ${key}: ${data1[key]}`);
     }
   });
 
---
+++ src/parser.js
@@ -0,0 +1,14 @@
+import yaml from 'js-yaml';
+import path from 'path';
+import { readFile } from './utilities.js';
+
+export default (filename) => {
+  const data = readFile(filename);
+
+  if (path.extname(filename).toLowerCase() === '.json') {
+    return JSON.parse(data.toString());
+  } if (['.yml', '.yaml'].includes(path.extname(filename).toLowerCase())) {
+    return yaml.load(data);
+  }
+  return null;
+};
