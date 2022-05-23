f803493110f996a16235fcd0d8a314aa05fec69c
Frit027 <Frit_027@mail.ru>
2022-05-23 15:00:12 +0300

    string addition replaced with array filling

--- src/index.js
+++ src/index.js
@@ -3,26 +3,28 @@ import fs from 'fs';
 import path from 'path';
 
 export default (file1, file2) => {
-  const data1 = fs.readFileSync(path.resolve(file1), 'utf8');
-  const data2 = fs.readFileSync(path.resolve(file2), 'utf8');
+  const data1 = fs.readFileSync(path.resolve(file1), 'utf-8');
+  const data2 = fs.readFileSync(path.resolve(file2), 'utf-8');
 
   const json1 = JSON.parse(data1);
   const json2 = JSON.parse(data2);
   const keys = _.uniq([...Object.keys(json1), ...Object.keys(json2)]).sort();
-  let diff = '{';
+  const arr = [];
 
   keys.forEach((key) => {
     if (key in json1 && !(key in json2)) {
-      diff += `\n  - ${key}: ${json1[key]}`;
+      arr.push(`  - ${key}: ${json1[key]}`);
     } else if (!(key in json1) && key in json2) {
-      diff += `\n  + ${key}: ${json2[key]}`;
+      arr.push(`  + ${key}: ${json2[key]}`);
     } else if (json1[key] !== json2[key]) {
-      diff += `\n  - ${key}: ${json1[key]}`;
-      diff += `\n  + ${key}: ${json2[key]}`;
+      arr.push(`  - ${key}: ${json1[key]}`);
+      arr.push(`  + ${key}: ${json2[key]}`);
     } else {
-      diff += `\n    ${key}: ${json1[key]}`;
+      arr.push(`    ${key}: ${json1[key]}`);
     }
   });
 
-  console.log(`${diff}\n}`);
+  // console.log(arr.length ? `{\n${arr.join('\n')}\n}` : '{}');
+  if (arr.length) return `{\n${arr.join('\n')}\n}`;
+  return '{}';
 };
