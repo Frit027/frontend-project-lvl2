ee8c4ea3d975099a457f3c7c1ed83dc5b43af7f2
Frit027 <Frit_027@mail.ru>
2022-05-25 16:08:16 +0300

    added variables

--- src/index.js
+++ src/index.js
@@ -14,9 +14,7 @@ export default (file1, file2) => {
 
       Object.keys(data).forEach((key) => {
         if (typeof data[key] !== 'object' || !data[key]) arr.push(`${replacer.repeat(count + innerCount)}${key}: ${data[key]}`);
-        else {
-          arr.push(`${replacer.repeat(count + innerCount)}${key}: ${getStrObject(data[key], count + innerCount)}`);
-        }
+        else arr.push(`${replacer.repeat(count + innerCount)}${key}: ${getStrObject(data[key], count + innerCount)}`);
       });
       return `{\n${arr.join('\n')}\n${replacer.repeat(innerCount - spacesCount + count)}}`;
     };
@@ -25,24 +23,27 @@ export default (file1, file2) => {
     const arr = [];
 
     keys.forEach((key) => {
+      const sameKey = `${replacer.repeat(count)}${key}`;
+      const plusKey = `${getIndent(replacer.repeat(count), '+')}${key}`;
+      const minusKey = `${getIndent(replacer.repeat(count), '-')}${key}`;
+
       if (key in data1 && !(key in data2)) {
-        arr.push(`${getIndent(replacer.repeat(count), '-')}${key}: ${getStrObject(data1[key])}`);
+        arr.push(`${minusKey}: ${getStrObject(data1[key])}`);
       } else if (!(key in data1) && key in data2) {
-        arr.push(`${getIndent(replacer.repeat(count), '+')}${key}: ${getStrObject(data2[key])}`);
+        arr.push(`${plusKey}: ${getStrObject(data2[key])}`);
       } else if (typeof data1[key] !== 'object' && typeof data2[key] !== 'object') {
-        if (data1[key] === data2[key]) arr.push(`${replacer.repeat(count)}${key}: ${data1[key]}`);
-        else {
-          arr.push(`${getIndent(replacer.repeat(count), '-')}${key}: ${data1[key]}`);
-          arr.push(`${getIndent(replacer.repeat(count), '+')}${key}: ${data2[key]}`);
-        }
+        if (data1[key] !== data2[key]) {
+          arr.push(`${minusKey}: ${data1[key]}`);
+          arr.push(`${plusKey}: ${data2[key]}`);
+        } else arr.push(`${sameKey}: ${data1[key]}`);
       } else if (typeof data1[key] === 'object' && typeof data2[key] !== 'object') {
-        arr.push(`${getIndent(replacer.repeat(count), '-')}${key}: ${getStrObject(data1[key])}`);
-        arr.push(`${getIndent(replacer.repeat(count), '+')}${key}: ${data2[key]}`);
+        arr.push(`${minusKey}: ${getStrObject(data1[key])}`);
+        arr.push(`${plusKey}: ${data2[key]}`);
       } else if (typeof data1[key] !== 'object' && typeof data2[key] === 'object') {
-        arr.push(`${getIndent(replacer.repeat(count), '-')}${key}: ${data1[key]}`);
-        arr.push(`${getIndent(replacer.repeat(count), '+')}${key}: ${getStrObject(data2[key])}`);
+        arr.push(`${minusKey}: ${data1[key]}`);
+        arr.push(`${plusKey}: ${getStrObject(data2[key])}`);
       } else {
-        arr.push(`${replacer.repeat(count)}${key}: ${getDiff(data1[key], data2[key], spacesCount + count)}`);
+        arr.push(`${sameKey}: ${getDiff(data1[key], data2[key], spacesCount + count)}`);
       }
     });
 
