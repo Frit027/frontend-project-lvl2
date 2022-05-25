e8511b6dc01e66873523d95367f93ff5f4e658c8
Frit027 <Frit_027@mail.ru>
2022-05-25 17:31:46 +0300

    added formatter

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -2,5 +2,11 @@ const REPLACER = ' ';
 
 const getIndent = (str, sign) => str.replace(/.(?=.$)/, sign);
 
-export const getLine = (key, value, count, sign = ' ') => `${getIndent(REPLACER.repeat(count), sign)}${key}: ${value}`;
-export const getTree = (arr, count) => `{\n${arr.join('\n')}\n${REPLACER.repeat(count)}}`;
+export default () => {
+  const getLine = (key, value, count, sign = ' ') => `${getIndent(REPLACER.repeat(count), sign)}${key}: ${value}`;
+  const getTree = (arr, count) => `{\n${arr.join('\n')}\n${REPLACER.repeat(count)}}`;
+  return {
+    line: getLine,
+    tree: getTree,
+  };
+};
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import _ from 'lodash';
 import parser from './parser.js';
-import { getLine, getTree } from './formatters/stylish.js';
+import stylish from './formatters/stylish.js';
 
-export default (file1, file2) => {
+export default (file1, file2, formatter = stylish) => {
   const getDiff = (data1, data2, count = 4) => {
     const spacesCount = 4;
 
@@ -10,26 +10,26 @@ export default (file1, file2) => {
       if (typeof data !== 'object' || !data) return data;
 
       const arr = Object.keys(data).map((key) => {
-        if (typeof data[key] !== 'object' || !data[key]) return getLine(key, data[key], count + innerCount);
-        return getLine(key, getStr(data[key], count + innerCount), count + innerCount);
+        if (typeof data[key] !== 'object' || !data[key]) return formatter().line(key, data[key], count + innerCount);
+        return formatter().line(key, getStr(data[key], count + innerCount), count + innerCount);
       });
-      return getTree(arr, innerCount - spacesCount + count);
+      return formatter().tree(arr, innerCount - spacesCount + count);
     };
 
     const keys = _.uniq([...Object.keys(data1), ...Object.keys(data2)]).sort();
     const arr = keys.map((key) => {
-      if (key in data1 && !(key in data2)) return getLine(key, getStr(data1[key]), count, '-');
-      if (!(key in data1) && key in data2) return getLine(key, getStr(data2[key]), count, '+');
+      if (key in data1 && !(key in data2)) return formatter().line(key, getStr(data1[key]), count, '-');
+      if (!(key in data1) && key in data2) return formatter().line(key, getStr(data2[key]), count, '+');
       if (typeof data1[key] !== 'object' && typeof data2[key] !== 'object' && data1[key] === data2[key]) {
-        return getLine(key, data1[key], count);
+        return formatter().line(key, data1[key], count);
       }
       if (typeof data1[key] === 'object' && typeof data2[key] === 'object') {
-        return getLine(key, getDiff(data1[key], data2[key], spacesCount + count), count);
+        return formatter().line(key, getDiff(data1[key], data2[key], spacesCount + count), count);
       }
-      return `${getLine(key, getStr(data1[key]), count, '-')}\n${getLine(key, getStr(data2[key]), count, '+')}`;
+      return `${formatter().line(key, getStr(data1[key]), count, '-')}\n${formatter().line(key, getStr(data2[key]), count, '+')}`;
     });
 
-    return getTree(arr, count - spacesCount);
+    return formatter().tree(arr, count - spacesCount);
   };
 
   // console.log(getDiff(parser(file1), parser(file2)));
