50504a68f84d13acf4f3cbaa1753b4368e968322
Frit027 <Frit_027@mail.ru>
2022-06-09 11:47:28 +0300

    tree construction is separated from logic

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,12 +1,43 @@
 const REPLACER = ' ';
 
 const getIndent = (str, sign) => str.replace(/.(?=.$)/, sign);
+const getLine = (key, value, count, sign = ' ') => `${getIndent(REPLACER.repeat(count), sign)}${key}: ${value}`;
+const getTree = (arr, count) => `{\n${arr.join('\n')}\n${REPLACER.repeat(count)}}`;
 
-export default () => {
-  const getLine = (key, value, count, sign = ' ') => `${getIndent(REPLACER.repeat(count), sign)}${key}: ${value}`;
-  const getTree = (arr, count) => `{\n${arr.join('\n')}\n${REPLACER.repeat(count)}}`;
-  return {
-    line: getLine,
-    tree: getTree,
+const getDiff = (diff, count = 4) => {
+  const spacesCount = 4;
+
+  const getStr = (data, innerCount = 4) => {
+    if (typeof data !== 'object' || !data) return data;
+
+    const arr = Object.keys(data).map((key) => {
+      if (typeof data[key] !== 'object' || !data[key]) return getLine(key, data[key], count + innerCount);
+      return getLine(key, getStr(data[key], count + innerCount), count + innerCount);
+    });
+    return getTree(arr, innerCount - spacesCount + count);
   };
+
+  const arr = diff.map((node) => {
+    const {
+      key, action, value, oldValue, newValue,
+    } = node;
+
+    switch (action) {
+      case 'added':
+        return getLine(key, getStr(value), count, '+');
+      case 'deleted':
+        return getLine(key, getStr(value), count, '-');
+      case 'changed':
+        return `${getLine(key, getStr(oldValue), count, '-')}\n${getLine(key, getStr(newValue), count, '+')}`;
+      case 'unchanged':
+        return getLine(key, value, count);
+      case 'children':
+        return getLine(key, getDiff(value, spacesCount + count), count);
+      default:
+        throw new TypeError('Неопознанное действие.');
+    }
+  });
+  return getTree(arr, count - spacesCount);
 };
+
+export default getDiff;
--- src/index.js
+++ src/index.js
@@ -1,37 +1,23 @@
 import _ from 'lodash';
 import parser from './parser.js';
-import stylish from './formatters/stylish.js';
+import formatter from './formatters/index.js';
 
-export default (file1, file2, formatter = stylish) => {
-  const getDiff = (data1, data2, count = 4) => {
-    const spacesCount = 4;
+export default (file1, file2, format = 'stylish') => {
+  const diffBuilder = (data1, data2) => {
+    const keys = _.uniq([..._.keys(data1), ..._.keys(data2)]).sort();
 
-    const getStr = (data, innerCount = 4) => {
-      if (typeof data !== 'object' || !data) return data;
-
-      const arr = Object.keys(data).map((key) => {
-        if (typeof data[key] !== 'object' || !data[key]) return formatter().line(key, data[key], count + innerCount);
-        return formatter().line(key, getStr(data[key], count + innerCount), count + innerCount);
-      });
-      return formatter().tree(arr, innerCount - spacesCount + count);
-    };
-
-    const keys = _.uniq([...Object.keys(data1), ...Object.keys(data2)]).sort();
-    const arr = keys.map((key) => {
-      if (key in data1 && !(key in data2)) return formatter().line(key, getStr(data1[key]), count, '-');
-      if (!(key in data1) && key in data2) return formatter().line(key, getStr(data2[key]), count, '+');
-      if (typeof data1[key] !== 'object' && typeof data2[key] !== 'object' && data1[key] === data2[key]) {
-        return formatter().line(key, data1[key], count);
+    return keys.map((key) => {
+      if (!_.has(data1, key)) return { key, action: 'added', value: data2[key] };
+      if (!_.has(data2, key)) return { key, action: 'deleted', value: data1[key] };
+      if (_.isObject(data1[key]) && _.isObject(data2[key])) return { key, action: 'children', value: diffBuilder(data1[key], data2[key]) };
+      if (data1[key] !== data2[key]) {
+        return {
+          key, action: 'changed', oldValue: data1[key], newValue: data2[key],
+        };
       }
-      if (typeof data1[key] === 'object' && typeof data2[key] === 'object') {
-        return formatter().line(key, getDiff(data1[key], data2[key], spacesCount + count), count);
-      }
-      return `${formatter().line(key, getStr(data1[key]), count, '-')}\n${formatter().line(key, getStr(data2[key]), count, '+')}`;
+      return { key, action: 'unchanged', value: data1[key] };
     });
-
-    return formatter().tree(arr, count - spacesCount);
   };
 
-  // console.log(getDiff(parser(file1), parser(file2)));
-  return getDiff(parser(file1), parser(file2));
+  return formatter(format, diffBuilder(parser(file1), parser(file2)));
 };
