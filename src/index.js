be66db1d78c7add21db7bff2fc696bf383c25494
Frit027 <Frit_027@mail.ru>
2022-06-09 16:08:02 +0300

    fixed readable

--- src/index.js
+++ src/index.js
@@ -9,7 +9,9 @@ export default (file1, file2, format = 'stylish') => {
     return keys.map((key) => {
       if (!_.has(data1, key)) return { key, action: 'added', value: data2[key] };
       if (!_.has(data2, key)) return { key, action: 'deleted', value: data1[key] };
-      if (_.isObject(data1[key]) && _.isObject(data2[key])) return { key, action: 'children', value: diffBuilder(data1[key], data2[key]) };
+      if (_.isObject(data1[key]) && _.isObject(data2[key])) {
+        return { key, action: 'children', value: diffBuilder(data1[key], data2[key]) };
+      }
       if (data1[key] !== data2[key]) {
         return {
           key, action: 'changed', oldValue: data1[key], newValue: data2[key],
