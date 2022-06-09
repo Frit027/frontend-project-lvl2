0152fa418e98fa394d90d6a36306d486b4d0bb6b
Frit027 <Frit_027@mail.ru>
2022-06-09 14:49:51 +0300

    fixed program.format

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,11 +4,11 @@ import genDiff from '../src/index.js';
 
 program
   .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
   .option('-f, --format <type>', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
-  .description('Compares two configuration files and shows a difference.')
   .action((filename1, filename2) => {
-    console.log(genDiff(filename1, filename2, program.format));
+    console.log(genDiff(filename1, filename2, program.opts().format));
   });
 
 program.parse();
