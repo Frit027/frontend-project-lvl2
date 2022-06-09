1f55c92fa23f5a3f5f3ae9129df65ac6addf7272
Frit027 <Frit_027@mail.ru>
2022-06-09 18:39:58 +0300

    differences builder moved to a separate file

--- src/index.js
+++ src/index.js
@@ -1,25 +1,11 @@
-import _ from 'lodash';
-import parser from './parser.js';
-import formatter from './formatters/index.js';
+import parse from './parser.js';
+import visualize from './formatters/index.js';
+import getDiff from './diffBuilder.js';
 
 export default (file1, file2, format = 'stylish') => {
-  const diffBuilder = (data1, data2) => {
-    const keys = _.sortBy(_.uniq([..._.keys(data1), ..._.keys(data2)]));
+  const data1 = parse(file1);
+  const data2 = parse(file2);
+  const diff = getDiff(data1, data2);
 
-    return keys.map((key) => {
-      if (!_.has(data1, key)) return { key, action: 'added', value: data2[key] };
-      if (!_.has(data2, key)) return { key, action: 'deleted', value: data1[key] };
-      if (_.isObject(data1[key]) && _.isObject(data2[key])) {
-        return { key, action: 'children', value: diffBuilder(data1[key], data2[key]) };
-      }
-      if (data1[key] !== data2[key]) {
-        return {
-          key, action: 'changed', oldValue: data1[key], newValue: data2[key],
-        };
-      }
-      return { key, action: 'unchanged', value: data1[key] };
-    });
-  };
-
-  return formatter(format, diffBuilder(parser(file1), parser(file2)));
+  return visualize(diff, format);
 };
